7f37df11c8474313a38dd798ae9c4ea4fc6c5191
shouta0715 <kshouta0715@gmail.com>
2025-08-20 22:16:13 +0900

    feat: seup env and valibot

---
+++ apps/native/src/env/client.ts
@@ -0,0 +1,12 @@
+/* eslint-disable eslint-plugin-n/no-process-env */
+import { createEnv } from "@t3-oss/env-core";
+import * as v from "valibot";
+
+export const env = createEnv({
+  clientPrefix: "EXPO_PUBLIC_",
+  server: {},
+  client: {
+    EXPO_PUBLIC_API_URL: v.string(),
+  },
+  runtimeEnv: process.env,
+});
