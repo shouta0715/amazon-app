3c21aa857ca4e06ab99200032b32c48cccdc62f0
shouta0715 <kshouta0715@gmail.com>
2025-08-26 22:11:22 +0900

    chore import

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,6 +1,17 @@
-import { Stack } from "expo-router";
-
 import "@/styles/tailwind.css";
+import { Stack } from "expo-router";
+import React from "react";
+import { StyledStack } from "@/components/navigation/stack";
+import { Providers } from "@/features/providers";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Providers>
+      <StyledStack
+        contentClassName="bg-gray-100"
+        headerClassName="bg-dark text-white"
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </StyledStack>
+    </Providers>
+  );
 }
