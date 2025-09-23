114ca4cb5fbd7d18d275d0b0b4ef7bf202ee9a84
shouta0715 <kshouta0715@gmail.com>
2025-09-23 10:29:31 +0900

    feat: add clerk sign in implement

--- apps/native/src/features/providers/index.tsx
+++ apps/native/src/features/providers/index.tsx
@@ -1,14 +1,26 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
+import { env } from "@/env/client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const colorScheme = useColorScheme();
 
   const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
-  return <ThemeProvider value={theme}>{children}</ThemeProvider>;
+  return (
+    <ClerkProvider
+      publishableKey={env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <ClerkLoaded>
+        <ThemeProvider value={theme}>{children}</ThemeProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
